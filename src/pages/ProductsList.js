3755985295e651545f1098db8f092b2d8285ea27
Vinicius Santiago <vinicius.santiago997@gmail.com>
2022-10-07 18:28:53 -0300

    Requisito 3 e 4

--- src/pages/ProductsList.js
+++ src/pages/ProductsList.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ButtonCart from '../components/ButtonCart';
 import SideBar from '../components/SideBar/SideBar';
+import '../components/SideBar/style.css';
 
 class ProductsList extends React.Component {
   render() {
     return (
-      <header data-testid="home-initial-message">
-        <h2> Digite algum termo de pesquisa ou escolha uma categoria. </h2>
+      <>
+        <header data-testid="home-initial-message">
+          <h2> Digite algum termo de pesquisa ou escolha uma categoria. </h2>
+          <ButtonCart />
+        </header>
         <SideBar />
-        <ButtonCart />
-      </header>
+      </>
     );
   }
 }
