1697332aae7a23be79f1d52ef7efc6f51162f50c
Vinicius Santiago <vinicius.santiago997@gmail.com>
2022-10-07 18:16:15 -0300

    Requisito 3 e 4

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ProductsList from './pages/ProductsList';
+import Cart from './pages/Cart';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={ ProductsList } />
+        <Route exact path="/shoppingcart" component={ Cart } />
       </Switch>
     </BrowserRouter>
   );
--- src/pages/ProductsList.js
+++ src/pages/ProductsList.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import ButtonCart from '../components/ButtonCart';
+import SideBar from '../components/SideBar/SideBar';
 
 class ProductsList extends React.Component {
   render() {
     return (
-      <header
-        data-testid="home-initial-message"
-      >
+      <header data-testid="home-initial-message">
         <h2> Digite algum termo de pesquisa ou escolha uma categoria. </h2>
+        <SideBar />
+        <ButtonCart />
       </header>
     );
   }
