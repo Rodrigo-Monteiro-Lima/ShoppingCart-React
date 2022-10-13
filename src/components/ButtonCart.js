e470747f5cbea571d4a87c599590980165c5634b
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-10-13 15:59:01 -0300

    cart number

--- src/components/ButtonCart.js
+++ src/components/ButtonCart.js
@@ -13,7 +13,7 @@ class ButtonCart extends Component {
             src="https://cdn-icons-png.flaticon.com/512/34/34627.png"
             alt="Cart icon."
           />
-          <div className="counter">{cartCount}</div>
+          <div className="counter" data-testid="shopping-cart-size">{cartCount}</div>
         </Link>
       </div>
     );
